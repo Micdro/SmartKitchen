48b6327a18aea3a03b8d1f1ba5cd134e1523ee13
Michael Drougas <sehser5@hotmail.com>
2023-03-20 23:18:06 -0400

    Completed SmartKitchen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SmartKitchen kitchen = new SmartKitchen();
+        kitchen.setKitchenState(true, false, true);
+        kitchen.doKitchenWork();
     }
 }
+
+
